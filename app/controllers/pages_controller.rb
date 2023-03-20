60b1de0396200ceb2ab013f8216c564d33a53b54
Fanny <venezia.fanny@gmail.com>
2023-03-20 17:31:07 +0100

    Création routes + views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def qui
+  end
+
+  def rando
+  end
+
+  def animation
+  end
+
+  def nature
+  end
+
+  def photos
+  end
+
+  def tarifs
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # root "articles#index"
   root to: 'pages#home'
+  get 'qui', to: 'pages#qui'
+  get 'rando', to: 'pages#rando'
+  get 'animation', to: 'pages#animation'
+  get 'nature', to: 'pages#nature'
+  get 'photos', to: 'pages#photos'
+  get 'tarifs', to: 'pages#tarifs'
+  get 'contact', to: 'pages#contact'
 end

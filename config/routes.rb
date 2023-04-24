7c290b8bb0370dde7d446475c3e28eb7da2ae65c
Fanny <venezia.fanny@gmail.com>
2023-04-24 17:59:50 +0200

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'pages#home'
   get 'qui', to: 'pages#qui'
-  get 'rando', to: 'pages#rando'
   get 'animation', to: 'pages#animation'
   get 'photos', to: 'pages#photos'
   get 'tarifs', to: 'pages#tarifs'
@@ -11,4 +10,6 @@ Rails.application.routes.draw do
   resources :messages, only: [:new, :create]
 
   resources :reservations, only: [:new, :create]
+
+  resources :hikes, only: [:index, :show]
 end

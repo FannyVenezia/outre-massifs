898057e8c49d4ca62d2ff64eb491fca2944b1d23
Fanny <venezia.fanny@gmail.com>
2023-04-26 12:00:40 +0200

    Refonte navbar avec animation scroll

---
+++ app/javascript/controllers/scrolldown_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="scrolldown"
+export default class extends Controller {
+  connect() {
+  }
+}
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);

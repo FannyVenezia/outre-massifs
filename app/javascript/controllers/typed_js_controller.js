bfe528251a8f83192cdef6465607248c2b2f9365
Fanny <venezia.fanny@gmail.com>
2023-04-27 17:19:47 +0200

    Ajout font et modification des tailles de police

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 import Typed from "typed.js"
 
-// Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Never forget movies anymore", "Create your movie-list library"],
+      strings: ["Change your life", "Learn to code"],
       typeSpeed: 50,
       loop: true
     })

109c5d0804f69ff2262f29d059b8826b288db97d
Fanny <venezia.fanny@gmail.com>
2023-04-27 18:01:00 +0200

    Modification syntaxe fonts

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module OutreMassifs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
